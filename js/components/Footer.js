d202d923115b7d67083f0fec786ef2ad496722ef
chengfengliu <772309659@qq.com>
2019-10-05 10:27:16 +0800

    feat: 页脚增加github地址

--- js/components/Footer.js
+++ js/components/Footer.js
@@ -4,7 +4,7 @@ export default class Footer extends Component {
   render() {
     return(
         <footer id="footer">
-          <p><span>1902 - 2019 ©NWU</span><span>QQ:1480712353</span></p>
+          <p><span>1902 - 2019 ©NWU</span><span>QQ:1480712353</span><span><a href="https://github.com/chengfengliu/nwu-knowleadge" target="_blank">Github</a></span></p>
         </footer>
     )
   }
