860470b1b09a937d06122923d7a0847b2ed63c29
chengfengliu <772309659@qq.com>
2020-02-18 15:30:10 +0800

    feat: 增加备案号

--- js/components/Footer.js
+++ js/components/Footer.js
@@ -4,7 +4,7 @@ export default class Footer extends Component {
   render() {
     return(
         <footer id="footer">
-          <p><span>1902 - 2019 ©NWU</span><span>QQ:1480712353</span><span><a href="https://github.com/chengfengliu/nwu-knowleadge" target="_blank">Github</a></span></p>
+          <p><span>1902 - 2019 ©NWU</span><span>QQ:1480712353</span><span><a href="https://github.com/chengfengliu/nwu-knowleadge" target="_blank">Github </a><a href="http://www.beian.miit.gov.cn/">陕ICP备18007808号-1</a></span></p>
         </footer>
     )
   }
